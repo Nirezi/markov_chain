201d80b799476074daa4deae59fe4bb6aabaadad
disneyresidents <disneyresidents@gmail.com>
2020-12-07 02:11:55 +0900

    [Add]サーバ参加時に初期のモデルファイルを生成しておく

---
+++ cogs/bot_join.py
@@ -0,0 +1,27 @@
+import asyncio
+import json
+
+from discord.ext import commands
+import MeCab
+import markovify
+
+
+class BotJoin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.lock = asyncio.Lock()
+        self.tagger = MeCab.Tagger("-Owakati")
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        text = self.tagger.parse("今日はいい天気ですね。")
+        model = markovify.Text(text, state_size=2)
+
+        async with self.lock:  # 同時アクセスを防ぐ
+            with open(f"model{guild.id}.json", "w") as f:
+                model = model.to_json()
+                json.dump(model, f)
+
+
+def setup(bot):
+    bot.add_cog(BotJoin(bot))
