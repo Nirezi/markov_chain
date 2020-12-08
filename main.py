cbc98f249eecf8d9cdab37cc7cb02328e3ab9b44
disneyresidents <disneyresidents@gmail.com>
2020-12-08 14:07:12 +0900

    [Add]codingなどの記述を追加

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+# coding:utf-8
 import os
 import traceback
 
@@ -18,7 +20,7 @@ class Bot(commands.Bot):
         super().__init__(command_prefix=prefix, allowed_mentions=allowed_mentions, intents=intents, help_command=None)
 
         self.load_extension('jishaku')
-        path = "./cogs" if os.name == "nt" else "~/markov_chain/cogs"  # 実行環境によってpathを変える
+        path = "./cogs" if os.name == "nt" else "./cogs"  # 実行環境によってpathを変える
         for file in os.listdir(path):
             if file.endswith(".py"):
                 try:
